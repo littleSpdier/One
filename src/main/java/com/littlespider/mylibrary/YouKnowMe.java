3fc646ea095921a8dfaa68f09ebc429b3a79f6be
littlespider <15172144011@163.com>
2018-12-03 17:56:51 +0800

    record

--- src/main/java/com/littlespider/mylibrary/YouKnowMe.java
+++ src/main/java/com/littlespider/mylibrary/YouKnowMe.java
@@ -1,7 +1,13 @@
 package com.littlespider.mylibrary;
 
+import android.util.Log;
+
 /**
  * Created by littlespider on 2018/12/03.
  */
 public class YouKnowMe {
+
+    public static void record(){
+        Log.i("prince", "read me");
+    }
 }
